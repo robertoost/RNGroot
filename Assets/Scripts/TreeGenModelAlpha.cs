3c6ecb9f2dccc20ee14f9586971da9c6601ab610
robertoost <github@robertoo.st>
2024-04-22 12:36:10 +0200

    feat(TreeGenModelAlpha): add modules for branching rules, metrics

--- Assets/Scripts/TreeGenModelAlpha.cs
+++ Assets/Scripts/TreeGenModelAlpha.cs
@@ -7,16 +7,22 @@ namespace RNGroot
     public class TreeGenModelAlpha : GrowthAlgorithm
     {
         public IEnvironmentalInput environmentalInput;
+        public IBranchingRules branchingRules;
+        public TreeMetrics treeMetrics;
         private List<Node> addedNodes = new List<Node>();
 
         protected override void Start()
         {
             environmentalInput = new SpaceColonization(tree, new SphereTreeEnvelope());
+            branchingRules = new RandomTreeBranchingRules();
             base.Start();
         }
 
         private void OnDrawGizmosSelected()
         {
+
+            // For now, show space colonization markers here.
+            //
             if (environmentalInput == null) return;
 
             SpaceColonization spaceCol = ((SpaceColonization)environmentalInput);
@@ -25,11 +31,11 @@ namespace RNGroot
             {
                 Gizmos.DrawSphere(spaceCol.markers[marker_id], 0.07f);
             }
-            //Gizmos.color = Color.red;
-            //foreach(int marker_id in spaceCol.marker_occupation.Keys)
-            //{
-            //    Gizmos.DrawSphere(spaceCol.markers[marker_id], 0.05f);
-            //}
+            Gizmos.color = Color.red;
+            foreach (int marker_id in spaceCol.marker_occupation.Keys)
+            {
+                Gizmos.DrawSphere(spaceCol.markers[marker_id], 0.05f);
+            }
         }
 
         public override void Grow()
@@ -73,44 +79,10 @@ namespace RNGroot
 
             // TODO: L-System for bud placement
             // Place buds.
-            AddRandomBuds(addedNodes);
+            branchingRules.PlaceBuds(tree, addedNodes);
 
+            treeMetrics = TreeMetricHelper.CalculateMetrics(tree);
             // TODO: Grow / leave dormant buds lower
-        }
-        private void AddRandomBuds(List<Node> chosenNodes)
-        {
-            foreach (Node node in tree.nodes)
-            {
-                if (node.childBuds.Count == 0 && node.terminal == true)
-                {
-
-                    // Generate a random direction based on the node direction, and rotate by it.
-                    //
-                    Vector3 randomDir = new Vector3(Random.value, Random.value, Random.value);
-                    randomDir = Vector3.Normalize(randomDir);
-                    float angle = Random.Range(20, 35);
-                    // Random positive or negative
-                    int posNeg = Random.Range(0, 2) * 2 - 1;
-
-                    Vector3 nodeDirection = node.parentNode != null ? node.Direction() : Vector3.up;
-                    Vector3 rotationAxis = Vector3.Cross(randomDir, nodeDirection);
-
-                    // Rotate by random axis and angle along node direction
-                    Vector3 newDir = Quaternion.AngleAxis(angle, rotationAxis) * nodeDirection;
-                    Vector3 newDirB = Quaternion.AngleAxis(-angle, rotationAxis) * nodeDirection;
-
-                    // Randomly chooses to add a second lateral bud or not.
-
-                    tree.AddBud(node, newDir);
-
-                    if (Random.value > 0.3)
-                    {
-                        tree.AddBud(node, newDirB);
-                    }
-
-                    node.terminal = false;
-                }
-            }
         }
     }
 }
